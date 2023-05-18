c88102ec08a5b0e60fa45d92c4aac9d8c529da1a
radthenone <krzyzak.codes@gmail.com>
2023-05-18 14:44:13 +0200

    create posts get method

---
+++ src/features/posts/components/Posts.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPosts } from '../data/data';
+
+export const Posts = () => {
+  const {
+    status,
+    error,
+    data: posts,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+  });
+
+  if (status === 'loading') return <h1>Loading...</h1>;
+
+  if (status === 'error') return <h1>{JSON.stringify(error)}</h1>;
+
+  return (
+    <div className="Posts">
+      <ol>
+        {posts.map((post: { id: number; title: string }) => {
+          return (
+              <div key={post.id}>
+                <a href={`/post/${post.id}`}><li>{post.title}</li></a>
+              </div>
+          )
+        })}
+      </ol>
+    </div>
+  );
+}
