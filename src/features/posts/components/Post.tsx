9a8bd0014b9932d38a1d652c74b7515cf00e6fd8
radthenone <krzyzak.codes@gmail.com>
2023-05-18 14:44:23 +0200

    create post get method

---
+++ src/features/posts/components/Post.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPost } from '../data/data';
+import {useParams} from "react-router-dom";
+
+export const Post = () => {
+    const { id } = useParams()
+    const {
+        status: statusPost,
+        error: errorPost,
+        data: post,
+    } = useQuery({
+        queryKey: ['posts', id && parseInt(id, 10)],
+        queryFn: () => id && getPost(parseInt(id, 10)),
+    })
+
+if ( statusPost === "loading") return <h1>Loading...</h1>
+if ( statusPost === "error") return <h1>{JSON.stringify(errorPost)}</h1>
+
+return (
+    <div>
+        <a href="/posts/" ><h1>All posts</h1></a>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+
+    </div>
+)
+}
+
